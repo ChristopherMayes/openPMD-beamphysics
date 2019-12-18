6a8f7c888d0e66c1f8f6e07c774b553a41d8f8b8
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2019-12-17 17:37:56 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+
+setup(
+    name='pmd-beamphysics',
+    version = 'v0.1-alpha',
+    packages = ['pmd_beaphysics'],
+    long_description=open('README.md').read()
+)

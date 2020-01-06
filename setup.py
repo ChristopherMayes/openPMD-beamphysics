3867be960ea5e7d91fc89639eeb0e4a58f08e361
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2020-01-06 13:48:02 -0800

    Fleshing out setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from os import path, environ
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
+    requirements = f.read().split()
 
 
 
 setup(
     name='pmd-beamphysics',
     version = 'v0.1-alpha',
-    packages = ['pmd_beaphysics'],
-    long_description=open('README.md').read()
+    #packages = ['pmd_beamphysics'],
+    packages=find_packages(exclude=['opmd_beamphysics']),  # This is the old package
+    package_dir={'pmd_beamphysics':'pmd_beamphysics'},
+    url='https://github.com/ChristopherMayes/openPMD-beamphysics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='>=3.7'
 )

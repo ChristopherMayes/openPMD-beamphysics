aed345db22284ed9e1ed4c818a9c3aa80e38471c
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2020-04-25 12:44:41 -0700

    Added elegant_h5_to_data

--- pmd_beamphysics/interfaces/elegant.py
+++ pmd_beamphysics/interfaces/elegant.py
@@ -1,4 +1,9 @@
+from pmd_beamphysics.units import m_e
+
 import numpy as np
+import os
+
+from h5py import File
 
 def write_elegant(particle_group,           
                outfile,
@@ -64,3 +69,95 @@ def write_elegant(particle_group,
       
     
     return outfile
+
+
+
+def elegant_h5_to_data(h5, group='page1', species='electron'):
+    """
+    Converts elegant data from an h5 handle or file to data for openPMD-beamphysics
+    
+    Elegant h5 has datasets:
+    
+    x, xp, y, xp, p=beta*gamma, t
+    m,  1,   m, 1, 1, s
+    
+    Momentum are reconstructed as:
+    
+    pz = p / sqrt(1+xp^2 + yp^2)
+    px = xp * pz
+    py = yp * pz
+    
+    In s-based codes, z=0 by definition. 
+    
+    For now, only electrons are allowed.
+    
+    Units are checked. 
+    
+    All particles are assumed to be live (status = 1)
+
+    TODO: More species. 
+        
+    """
+    
+    # Allow for opening a file
+    if isinstance(h5, str):
+        assert os.path.exists(h5), f'File does not exist: {h5}'
+        h5 = File(h5, 'r')
+        
+    if group:
+        g = h5[group]
+    else:
+        g = h5
+        
+    assert species=='electron', f'{species} not allowed yet. Only electron is implemented.'    
+    mc2 = m_e
+        
+    # These should exist
+    col = g['columns']
+    par = g['parameters']
+    
+    p = col['p'][:]*mc2
+    xp = col['xp'][:]
+    yp = col['yp'][:]
+    pz = p/np.sqrt(1 + xp**2 + yp**2)
+    px = xp * pz
+    py = yp * pz
+    
+
+    # Check charge unit
+    charge = par['Charge'][0]
+    charge_unit = par['Charge'].attrs['units']
+    assert charge_unit == b'C', 'Expected C as unit for Charge'
+    
+    # Check dataset units
+    expected_units = {
+        'p':b'm$be$nc',
+        'xp':b'',
+        'yp':b'',
+        'x':b'm',
+        'y':b'm',
+        't':b's'
+    }    
+    for c, v in expected_units.items():
+        u = col[c].attrs['units']
+        assert u==v, f'Dataset {c} units expected to have {v}, but have {u}'
+    
+    # number of particles
+    n = len(p)
+    
+    status=1
+    data = {
+        'x':col['x'][:],
+        'y':col['y'][:],
+        'z':np.full(n, 0),
+        'px':px,
+        'py':py,
+        'pz':pz,
+        't': col['t'][:],
+        'status': np.full(n, status),
+        'species':species,
+        'weight': np.full(n, abs(charge)/n),
+        'id':  col['particleID'][:]
+    }
+    return data
+

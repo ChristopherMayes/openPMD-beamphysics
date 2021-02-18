e226db480ceb71f01818d5a6366f23d986aaaf6e
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-02-17 21:30:37 -0800

    Added version

--- pmd_beamphysics/__init__.py
+++ pmd_beamphysics/__init__.py
@@ -2,3 +2,5 @@ from .particles import ParticleGroup, single_particle
 from .fields.fieldmesh import FieldMesh
 from .readers import particle_paths
 from .writers import pmd_init
+
+from ._version import __version__

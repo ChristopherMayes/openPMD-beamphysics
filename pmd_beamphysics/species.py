af5db350ef59f401426b349a3979f66751e2c511
Colwyn Gulliford <36416205+ColwynGulliford@users.noreply.github.com>
2024-06-29 21:17:09 -0400

    Update species for H2+

--- pmd_beamphysics/species.py
+++ pmd_beamphysics/species.py
@@ -11,25 +11,33 @@ mpc2 = scipy.constants.value('proton mass energy equivalent in MeV')*1e6
 
 mhmc2 = mpc2 + mec2 * 2 # H- mass energy equivalent in MeV
 
+mH2pc2 = 2*mpc2 + mec2 # Molecular Hydrogen Ion H2+
+
 e_charge = scipy.constants.e
 c_light = scipy.constants.c
 
 CHARGE_OF = {'electron': -e_charge,
             'positron': e_charge,
             'proton': e_charge,
-            'H-': -e_charge}
+            'H-': -e_charge,
+            'H2+': e_charge,
+            }
 
 CHARGE_STATE = {
     'electron': -1,
     'positron': 1,
     'proton': 1,
-    'H-': -1}
+    'H-': -1,
+    'H2+': +1,
+    }
 
 
 MASS_OF = {'electron': mec2,
            'positron': mec2,
            'proton': mpc2,
-           'H-': mhmc2}
+           'H-': mhmc2,
+           'H2+': mH2pc2,
+           }
 
 
 

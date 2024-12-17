FIX: genesis4 scaling factor

pmd_beamphysics/wavefront.py
@@ -1894,8 +1894,9 @@ class Wavefront:
         else:
             refposition = 0.0
 
+        v_over_m_to_genesis = gridsize / np.sqrt(2.0 * Z0)
         return FieldFile(
-            dfl=self.rmesh,
+            dfl=self.rmesh * v_over_m_to_genesis,
             param=FieldFileParams(
                 #  number of gridpoints in one transverse dimension equal to nx and ny above
                 gridpoints=nx,

tests/test_wavefront.py
@@ -198,10 +198,10 @@ def test_write_and_read_genesis4(
     # E           ?            ^       ^       ^            ^       ^       ^...
     # Should probably switch to `allclose` in `__eq__` but I think Chris has a
     # preference for true equality
-    # assert np.allclose(wavefront.rmesh.real, loaded.rmesh.real)
-    # assert np.allclose(wavefront.rmesh.imag, loaded.rmesh.imag)
-    # loaded._rmesh = wavefront.rmesh
-    # assert wavefront == loaded
+    assert np.allclose(wavefront.rmesh.real, loaded.rmesh.real)
+    assert np.allclose(wavefront.rmesh.imag, loaded.rmesh.imag)
+    loaded._rmesh = wavefront.rmesh
+    assert wavefront == loaded
 
 
 def test_write_and_read_openpmd(
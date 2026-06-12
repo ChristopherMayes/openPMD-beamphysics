09f2b7331723d829a73d68075f170650cce87c17
Christopher Mayes <31023527+ChristopherMayes@users.noreply.github.com>
2026-06-12 14:17:02 -0700

    Forgot to use weights in twiss dispersion calc, use p0c properly, initialize twiss arrays

--- beamphysics/statistics.py
+++ beamphysics/statistics.py
@@ -115,7 +115,7 @@ def matched_particles(
 ):
     """
     Perfoms simple Twiss 'matching' by applying a linear transformation to
-        x, px if plane == 'x', or x, py if plane == 'y'
+        x, px if plane == 'x', or y, py if plane == 'y'
 
     Returns a new ParticleGroup
 
@@ -243,16 +243,16 @@ def particle_twiss_dispersion(particle_group, plane="x", fraction=1, p0c=None):
 
     x = P[plane]
     xp = P["p" + plane] / p0c
-    delta = P["p"] / P["mean_p"]  # - 1
+    delta = P["p"] / p0c  # - 1
 
-    # Form covariance matrix
-    np.cov([x, xp, delta], aweights=P.weight)
+    # Form weighted covariance matrix
+    sigma = np.cov([x, xp, delta], aweights=P.weight)
 
     # Actual calc
-    twiss = twiss_dispersion_calc(np.cov([x, xp, delta]))
+    twiss = twiss_dispersion_calc(sigma)
 
     # Add norm
-    twiss["norm_emit"] = twiss["emit"] * P["mean_p"] / P.mass
+    twiss["norm_emit"] = twiss["emit"] * p0c / P.mass
 
     # Add suffix
     out = {}
@@ -444,7 +444,6 @@ def slice_statistics(particle_group, keys=["mean_z"], n_slice=40, slice_key=None
     normal_keys = set()
 
     for k in keys:
-        sdat[k] = np.empty(n_slice)
         if k.startswith("twiss"):
             if k == "twiss" or k == "twiss_xy":
                 twiss_planes.add("x")
@@ -454,6 +453,7 @@ def slice_statistics(particle_group, keys=["mean_z"], n_slice=40, slice_key=None
                 assert plane in ("x", "y")
                 twiss_planes.add(plane)
         else:
+            sdat[k] = np.empty(n_slice)
             normal_keys.add(k)
 
     twiss_plane = "".join(twiss_planes)  # flatten
